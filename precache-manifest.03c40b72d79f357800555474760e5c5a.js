0fc5acfb82c84689bec2e355f5240bd49e341efb
hidaseika <hidaseika@users.noreply.github.com>
2022-08-04 07:26:11 +0000

    Commit performed using Copy Push Files action

---
+++ precache-manifest.03c40b72d79f357800555474760e5c5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec1fa558a9b6e3c13b02",
+    "url": "/static/css/main.76a2b3dd.chunk.css"
+  },
+  {
+    "revision": "ec1fa558a9b6e3c13b02",
+    "url": "/static/js/main.ec1fa558.chunk.js"
+  },
+  {
+    "revision": "dd02a13f5f447c639dd3",
+    "url": "/static/js/1.dd02a13f.chunk.js"
+  },
+  {
+    "revision": "7b2f7f3bdeaae4779ade",
+    "url": "/static/js/2.7b2f7f3b.chunk.js"
+  },
+  {
+    "revision": "ca941f47c15d629b2007",
+    "url": "/static/js/runtime~main.ca941f47.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "6a68d63d3de72844a6a02aeffc53a02e",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e9637dd8779f47573d8e49ba79f3fc14.js"
+  "/precache-manifest.03c40b72d79f357800555474760e5c5a.js"
 );
 
 workbox.clientsClaim();
